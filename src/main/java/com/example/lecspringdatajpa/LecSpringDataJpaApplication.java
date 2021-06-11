a8dbff7ec5cc19813d33cf99b8b97a05cfde5ff5
hanbee1005 <sonhanbi1002@gmail.com>
2021-06-11 15:54:47 +0900

    init

---
+++ src/main/java/com/example/lecspringdatajpa/LecSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.lecspringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecSpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LecSpringDataJpaApplication.class, args);
+    }
+
+}
