81027ed638e7074b47bf151b2901dcaf712e47f2
hanbee1005 <sonhanbi1002@gmail.com>
2021-06-11 16:20:24 +0900

    postgres 드라이버 추가 및 entityManager 로 테스트 수행

---
+++ src/main/java/com/example/lecspringdatajpa/JpaRunner.java
@@ -0,0 +1,26 @@
+package com.example.lecspringdatajpa;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Component
+@Transactional
+public class JpaRunner implements ApplicationRunner {
+
+    @PersistenceContext
+    EntityManager entityManager;  // JPA 핵심 클래스
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Account account = new Account();
+        account.setUsername("keesun");
+        account.setPassword("jpa");
+
+        entityManager.persist(account);
+    }
+}
