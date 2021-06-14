06094000acbae80644b101b151b5b074bd967f34
hanbee1005 <sonhanbi1002@gmail.com>
2021-06-14 14:09:35 +0900

    직접 repository 작성 (임시)

---
+++ src/main/java/com/example/lecspringdatajpa/PostRepository.java
@@ -0,0 +1,29 @@
+package com.example.lecspringdatajpa;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+@Transactional
+public class PostRepository {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public Post add(Post post) {
+        entityManager.persist(post);
+        return post;
+    }
+
+    public void delete(Post post) {
+        entityManager.remove(post);
+    }
+
+    public List<Post> findAll() {
+        return entityManager.createQuery("SELECT p FROM Post AS p").getResultList();
+    }
+}
