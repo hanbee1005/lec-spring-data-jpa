f8ae0488136edb6fb57b2ad38bf4ab8576efc034
hanbee <sonhanbi1002@gmail.com>
2021-06-16 23:30:48 +0900

    PagingAndSortingRepository method 테스트

--- src/test/java/com/example/lecspringdatajpa/PostRepositoryTest.java
+++ src/test/java/com/example/lecspringdatajpa/PostRepositoryTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -39,6 +41,15 @@ class PostRepositoryTest {
         // Then
         assertEquals(1, posts.size());
         assertThat(posts).contains(newPost);
+
+        // When
+        Page<Post> page = postRepository.findAll(PageRequest.of(0, 10));
+
+        // Then
+        assertThat(page.getTotalElements()).isEqualTo(1);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getSize()).isEqualTo(10);
+        assertThat(page.getNumberOfElements()).isEqualTo(1);
     }
 
 }
