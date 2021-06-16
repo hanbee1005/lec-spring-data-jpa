35c8ddf667e45ee4fc2ccc4b4d8f96d275990b6f
hanbee <sonhanbi1002@gmail.com>
2021-06-16 20:24:45 +0900

    spring data jpa 적용하여 postRepository 생성 및 사용

--- src/main/java/com/example/lecspringdatajpa/JpaRunner.java
+++ src/main/java/com/example/lecspringdatajpa/JpaRunner.java
@@ -1,24 +1,20 @@
 package com.example.lecspringdatajpa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.List;
-
 @Component
 @Transactional
 public class JpaRunner implements ApplicationRunner {
 
-    @PersistenceContext
-    EntityManager entityManager;  // JPA 핵심 클래스
+    @Autowired
+    PostRepository postRepository;
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
-        List<Post> posts = entityManager.createNativeQuery("select * from Post", Post.class).getResultList();
-        posts.forEach(System.out::println);
+    public void run(ApplicationArguments args) {
+         postRepository.findAll().forEach(System.out::println);
     }
 }
--- src/main/java/com/example/lecspringdatajpa/PostRepository.java
+++ src/main/java/com/example/lecspringdatajpa/PostRepository.java
@@ -1,29 +1,7 @@
 package com.example.lecspringdatajpa;
 
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.List;
+public interface PostRepository extends JpaRepository<Post, Long> {
 
-@Repository
-@Transactional
-public class PostRepository {
-
-    @PersistenceContext
-    EntityManager entityManager;
-
-    public Post add(Post post) {
-        entityManager.persist(post);
-        return post;
-    }
-
-    public void delete(Post post) {
-        entityManager.remove(post);
-    }
-
-    public List<Post> findAll() {
-        return entityManager.createQuery("SELECT p FROM Post AS p").getResultList();
-    }
 }
