7f28dcdccc846c66fcaf74b4ceddf0fc7949a1af
hanbee1005 <sonhanbi1002@gmail.com>
2021-06-17 09:20:36 +0900

    메소드 이름이나 쿼리 애노테이션을 사용한 jpql로 쿼리 만들기

--- src/main/java/com/example/lecspringdatajpa/CommentRepository.java
+++ src/main/java/com/example/lecspringdatajpa/CommentRepository.java
@@ -1,4 +1,11 @@
 package com.example.lecspringdatajpa;
 
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 public interface CommentRepository extends MyRepository<Comment, Long> {
+
+    @Query("SELECT c from Comment as c")
+    List<Comment> findByTitleContains(String keyword);
 }
